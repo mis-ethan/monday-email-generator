10f611e0c0ce4e074751c35aaaf48aef74a85954
mis-ethan <ethan.green@ochsinc.org>
2025-07-28 15:33:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ function transformText(name, domain = "ochsinc.org") {
 }
 
 app.post('/generate-email', async (req, res) => {
-  const { itemId, sourceColumnId, targetColumnId } = req.body.payload?.inboundFieldValues;
+  const { itemId, sourceColumnId, targetColumnId } = req.body;
+  
   
   //console.log('Received request from Monday:', req.headers, req.body);
 
