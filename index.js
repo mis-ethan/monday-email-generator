041dce8cfe6570abf5f8ce699a2c5d0a98bd5bbd
mis-ethan <ethan.green@ochsinc.org>
2025-08-04 10:38:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -242,7 +242,11 @@ app.post('/loaner-fob', async (req, res) => {
       console.log('no fob with that number currently in system');
       return res.status(200).send('OK');
     }
-    else{console.log(fetchResponse2.data.data.boards.items_page)}
+    else{
+      console.log(fetchResponse2.data.data.boards);
+      console.log('/n');
+      console.log(fetchResponse2.data.data);
+    }
 
     //console.log(columns);
 
