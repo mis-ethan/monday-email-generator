ed9f4a9c84be88ab03b2f761e5226e602cffaecb
mis-ethan <ethan.green@ochsinc.org>
2025-08-12 15:24:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ app.post('/loaner-fob', async (req, res) => {
 
   // Step 1: Set up query
   const fetchQuery1 = `
-    query($itemId: [ID!]){
+    mutation($itemId: [ID!]){
       delete_item(item_id: $itemId) {
         name
         column_values {
